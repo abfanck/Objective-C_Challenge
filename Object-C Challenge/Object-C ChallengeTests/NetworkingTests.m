9045817fe03a90ba9aaa252d264cfb3b0dfb703b
Arthur Fanck <arthur.fanck@gmail.com>
2020-03-23 17:31:19 -0300

    runned first test

--- Object-C Challenge/Object-C ChallengeTests/NetworkingTests.m
+++ Object-C Challenge/Object-C ChallengeTests/NetworkingTests.m
@@ -1,5 +1,5 @@
 //
-//  Object_C_ChallengeTests.m
+//  NetworkingTests.m
 //  Object-C ChallengeTests
 //
 //  Created by Arthur Bastos Fanck on 23/03/20.
@@ -8,32 +8,36 @@
 #import <XCTest/XCTest.h>
 #import <Object-C Challenge/Networking/Networking.h>
 
-@interface Object_C_ChallengeTests : XCTestCase
+@interface NetworkingTests : XCTestCase
 
-@property (strong, nonatomic) Networking *net;
+@property (strong, nonatomic) Networking *network;
 
 @end
 
-@implementation Object_C_ChallengeTests
+@implementation NetworkingTests
 
 - (void)setUp {
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    [super setUp];
+    self.network = Networking.new;
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
 }
 
 - (void)testExample {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
+    XCTestExpectation *expectation = [self expectationWithDescription:@"asynchronous request"];
+    
+    [self.network fetchMovie:POPULAR completionHandler:^(NSMutableArray * _Nonnull array) {
+        XCTAssert(array, @"array nil");
+        [expectation fulfill];
     }];
+    
+    [self waitForExpectationsWithTimeout:10 handler:nil];
 }
 
 @end
